a84178bc35b2f4236e5e828373e863d89cfc93a6
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-03-18 19:57:08 -0400

    added dummy data for users

---
+++ users.js
@@ -0,0 +1,40 @@
+/* Seed Dummy Data file */
+
+module.exports = [
+  {
+    id: 1,
+    username: "BUBBLES",
+  },
+  {
+    id: 2,
+    username: "Pikachu",
+  },
+  {
+    id: 3,
+    username: "Casper",
+  },
+  {
+    id: 4,
+    username: "Scooby Doo",
+  },
+  {
+    id: 5,
+    username: "Master Jim",
+  },
+  {
+    id: 6,
+    username: "James",
+  },
+  {
+    id: 7,
+    username: "Waldo",
+  },
+  {
+    id: 8,
+    username: "Pooh Bear",
+  },
+  {
+    id: 9,
+    username: "Team Rocket",
+  },
+]
